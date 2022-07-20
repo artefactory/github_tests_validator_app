6cbdc9d224a5844ba8d589b2df6ce2e956e193a2
larryandria <larry.andriamampianina@artefact.com>
2022-07-20 12:20:21 +0200

    fix: typing function

--- github_tests_validator_app/utils.py
+++ github_tests_validator_app/utils.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Any, List
 
 import hashlib
 
@@ -6,7 +6,7 @@ from github import ContentFile, Github, Repository
 from github_tests_validator_app.constants import TESTS_FOLDER_NAME
 
 
-def get_hash_files(contents: list(ContentFile.ContentFile)) -> str:
+def get_hash_files(contents: List[ContentFile.ContentFile]) -> str:
     hash_sum = ""
     for content in contents:
         hash_sum += content.sha
@@ -15,26 +15,24 @@ def get_hash_files(contents: list(ContentFile.ContentFile)) -> str:
     return str(hash.hexdigest())
 
 
-def get_tests_hash(token: str, owner: str, repo_name: str, tests_folder=TESTS_FOLDER_NAME) -> str:
+def get_tests_hash(token: str, owner: str, repo_name: str) -> str:
     git_connection = Github(login_or_token=token)
     repo = git_connection.get_repo(f"{owner}/{repo_name}")
-    contents = repo.get_contents(tests_folder)
+    contents = repo.get_contents(TESTS_FOLDER_NAME)
     files_content = get_files_content(contents, repo)
     hash = get_hash_files(files_content)
     return hash
 
 
-def get_files_content(
-    contents: Union(list, ContentFile.ContentFile), repo: Repository.Repository
-) -> list(ContentFile.ContentFile):
-    files_content = []
-    if isinstance(contents, list):
+def get_files_content(contents: Any, repo: Repository.Repository) -> List[ContentFile.ContentFile]:
+    if isinstance(contents, ContentFile.ContentFile):
+        return [contents]
+    else:
+        files_content = []
         while contents:
             file_content = contents.pop(0)
             if file_content.type == "dir":
                 contents.extend(repo.get_contents(file_content.path))
             else:
                 files_content.append(file_content)
-    else:
-        return [contents]
-    return files_content
+        return files_content
