e6d3e0a4a6330ead7a70be41b2ed60f2558c4b36
larryandria <larry.andriamampianina@artefact.com>
2022-07-20 14:41:36 +0200

    feat: add function compare tests folder

--- github_tests_validator_app/utils.py
+++ github_tests_validator_app/utils.py
@@ -15,9 +15,7 @@ def get_hash_files(contents: List[ContentFile.ContentFile]) -> str:
     return str(hash.hexdigest())
 
 
-def get_tests_hash(token: str, owner: str, repo_name: str) -> str:
-    git_connection = Github(login_or_token=token)
-    repo = git_connection.get_repo(f"{owner}/{repo_name}")
+def get_tests_hash(repo: Repository.Repository) -> str:
     contents = repo.get_contents(TESTS_FOLDER_NAME)
     files_content = get_files_content(contents, repo)
     hash = get_hash_files(files_content)
@@ -25,14 +23,41 @@ def get_tests_hash(token: str, owner: str, repo_name: str) -> str:
 
 
 def get_files_content(contents: Any, repo: Repository.Repository) -> List[ContentFile.ContentFile]:
-    if isinstance(contents, ContentFile.ContentFile):
-        return [contents]
-    else:
-        files_content = []
-        while contents:
-            file_content = contents.pop(0)
-            if file_content.type == "dir":
-                contents.extend(repo.get_contents(file_content.path))
-            else:
-                files_content.append(file_content)
-        return files_content
+    files_content = []
+    while contents:
+        file_content = contents.pop(0)
+        if file_content.type == "dir":
+            contents.extend(repo.get_contents(file_content.path))
+        else:
+            files_content.append(file_content)
+    return files_content
+
+
+def get_repo(token: str, owner: str, repo_name: str) -> Repository.Repository:
+    git_connection = Github(login_or_token=token)
+    repo = git_connection.get_repo(f"{owner}/{repo_name}")
+    return repo
+
+
+def get_last_hash_commit(repo: Repository.Repository, branch_name: str) -> str:
+    branch = repo.get_branch(branch_name)
+    return branch.commit.sha
+
+
+def compare_tests_folder(
+    student_repo: Repository.Repository, solution_repo: Repository.Repository
+) -> bool:
+    student_contents = student_repo.get_contents(TESTS_FOLDER_NAME)
+
+    if (
+        isinstance(student_contents, ContentFile.ContentFile)
+        and student_contents.type == "submodule"
+    ):
+        solution_last_commit = get_last_hash_commit(solution_repo, "main")
+        student_tests_commit = student_contents.sha
+        return solution_last_commit == student_tests_commit
+
+    student_hash_tests = get_tests_hash(student_repo)
+    solution_hash_tests = get_tests_hash(solution_repo)
+
+    return student_hash_tests == solution_hash_tests
